8086449db734ccd427c5f0b62794a3c3f73c0b5b
Nicole Ann V. Tavares <84914337+YoussefMohamed-joe@users.noreply.github.com>
2024-04-22 16:35:06 +0200

    done with the model

--- posts_app/lib/core/api_servives.dart
+++ posts_app/lib/core/api_servives.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:posts_app/core/constants.dart';
 import 'package:posts_app/posts_model/posts_model.dart';
 import 'package:http/http.dart' as http;
@@ -7,6 +9,15 @@ class ApiServices {
 
   Future<List<PostsModel>> getPosts() async {
     final url = Uri.parse(ApiConstants.baseUrl+ApiConstants.post);
-
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonModel = jsonDecode(response.body) as List;
+      List<PostsModel> posts = jsonModel.map((e) {
+        return PostsModel.fromJson(e);
+      }).toList();
+      return posts;
+    } else {
+      throw Exception('Failed to load posts');
+    }
   }
 }
