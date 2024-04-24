67df1c60797c2e124d44808a65cc7c0522770f08
Nicole Ann V. Tavares <84914337+YoussefMohamed-joe@users.noreply.github.com>
2024-04-24 19:45:22 +0200

    sign in

--- charity_app/lib/features/upload/uploadview.dart
+++ charity_app/lib/features/upload/uploadview.dart
@@ -1,5 +1,6 @@
 import 'package:charity_app/core/utils/colors.dart';
 import 'package:charity_app/core/utils/text_styles.dart';
+import 'package:charity_app/features/widgets/custom_button.dart';
 import 'package:charity_app/features/widgets/custon_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -13,6 +14,7 @@ class UploadView extends StatefulWidget {
 
 class _UploadViewState extends State<UploadView> {
   bool passvis=true;
+  bool isChecked=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +22,7 @@ class _UploadViewState extends State<UploadView> {
       body: Center(
           child: Column(
         children: [
-          const Gap(80),
+          const Gap(50),
           Text('إحسان', style: getehsan()),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -36,7 +38,7 @@ class _UploadViewState extends State<UploadView> {
               ),
             ],
           ),
-          const Gap(50),
+          const Gap(30),
           Expanded(
             child: Container(
               width: double.infinity,
@@ -63,12 +65,12 @@ class _UploadViewState extends State<UploadView> {
                           ),
                         ],
                       ),
-                      const Gap(20),
+                      const Gap(10),
                       const CustomTextField(
                         label: 'Email',
                         prefix: Icons.email,
                       ),
-                      const Gap(20),
+                      const Gap(10),
                       CustomTextField(
                         label: 'Password',
                         obsecure: passvis,
@@ -80,6 +82,53 @@ class _UploadViewState extends State<UploadView> {
                           });
                         },
                       ),
+                      const Gap(10),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                isChecked = !isChecked;
+                              });
+                            },
+                            child: Container(
+                              width: 20,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: AppColors.black),
+                              ),
+                              child: isChecked?SizedBox():Icon(Icons.done,color: AppColors.black,size: 18,),
+                            ),
+                          ),
+                          const Gap(10),
+                          Text('Remember me',style: getbody(color: AppColors.dgrey),),
+                          const Spacer(),
+                          InkWell(onTap: (){},child: Text('Forgot Password?',style: getbody(color: AppColors.dgrey),)),
+                        ],
+                      ),
+                      const Gap(10),
+                      CustomButton(text: 'LOG IN',onpressed: (){
+                        //////////////
+                      },height: 47,width: double.infinity, ),
+                      const Gap(10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 30,
+                            height: 1.5,
+                            color: AppColors.dgrey,
+                          ),
+                          const Gap(10),
+                          Text('Or Continue With',style: getbody(color: AppColors.dgrey),),
+                          const Gap(10),
+                          Container(
+                            width: 30,
+                            height: 1.5,
+                            color: AppColors.dgrey,
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
---
+++ charity_app/lib/features/widgets/custom_button.dart
@@ -0,0 +1,27 @@
+import 'package:charity_app/core/utils/colors.dart';
+import 'package:charity_app/core/utils/text_styles.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key, required this.text, this.width, this.height, this.onpressed,
+  });
+  final String text;
+  final double? width;
+  final double? height;
+  final Function()? onpressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height?? 60,
+      width: width ?? 270,
+      child: ElevatedButton(onPressed: onpressed ?? (){}, 
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.green,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+      ),
+      child: Text(text,style: getbody(color: AppColors.white),)),
+    );
+  }
+}
