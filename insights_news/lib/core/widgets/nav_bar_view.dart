f8da2c7b9aca1a9cfb8409de7565a951db45c017
Nicole Ann V. Tavares <84914337+YoussefMohamed-joe@users.noreply.github.com>
2024-04-28 21:57:40 +0300

    finished navigate

--- insights_news/lib/core/widgets/nav_bar_view.dart
+++ insights_news/lib/core/widgets/nav_bar_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:insights_news/core/utils/colors.dart';
-
+import 'package:insights_news/features/home/home_view.dart';
+import 'package:insights_news/features/profile/profile_view.dart';
+import 'package:insights_news/features/search/search_view.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -11,25 +13,48 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  int currenindex = 0;
+  List<Widget> views = [
+    const HomeView(),
+    const SearchView(),
+    const ProfileView()
+  ];
   @override
   Widget build(BuildContext context) {
-    int currenindex = 0;
     return Scaffold(
+      body: views[currenindex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currenindex,
-        onTap: (value) {
-          setState(() {
-                currenindex = value;      
-          });
-        },
+          currentIndex: currenindex,
+          onTap: (value) {
+            setState(() {
+              currenindex = value;
+            });
+          },
           backgroundColor: Appcolors.blackbg,
           selectedItemColor: Appcolors.green,
           unselectedItemColor: Appcolors.white,
-          type: BottomNavigationBarType.fixed, 
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
           items: [
-            BottomNavigationBarItem(icon: SvgPicture.asset('assets/gome.svg'), label: 'Home'),
-            BottomNavigationBarItem(icon: SvgPicture.asset('assets/search.svg'), label: 'Search'),
-            BottomNavigationBarItem(icon: SvgPicture.asset('assets/Profile.svg'), label: 'Profile'),
+            BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset('assets/home.svg',
+                    colorFilter:
+                        ColorFilter.mode(Appcolors.green, BlendMode.srcIn)),
+                icon: SvgPicture.asset('assets/home.svg'),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset('assets/search.svg',
+                    colorFilter:
+                        ColorFilter.mode(Appcolors.green, BlendMode.srcIn)),
+                icon: SvgPicture.asset('assets/search.svg'),
+                label: 'Search'),
+            BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset('assets/Profile.svg',
+                    colorFilter:
+                        ColorFilter.mode(Appcolors.green, BlendMode.srcIn)),
+                icon: SvgPicture.asset('assets/Profile.svg'),
+                label: 'Profile'),
           ]),
     );
   }
--- insights_news/lib/features/home/home_view.dart
+++ insights_news/lib/features/home/home_view.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:insights_news/core/services/local_storage.dart';
 import 'package:insights_news/core/utils/colors.dart';
 import 'package:insights_news/core/utils/text_styles.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,51 +16,72 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int page = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: SafeArea(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hello ${AppLocalStorage.getData('name')}',
-                          style: getTitleStyle(color: Appcolors.white),
-                        ),
-                        Text(
-                          'Have a nice day',
-                          style: getBodyStyle(color: Appcolors.grey),
-                        ),
-                        const Spacer(),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(360),
-                          child: Image.file(
-                            File(AppLocalStorage.getData('path')),
-                            width: 60,
-                            height: 60,
-                            fit: BoxFit.cover,
-                            errorBuilder: (context, error, stackTrace) {
-                              return Image.asset(
-                                'assets/user.png',
-                                width: 50,
-                                color: Appcolors.blackbg,
-                              );
-                            },
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(children: [
+              Row(children: [
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    'Hello, ${AppLocalStorage.getData('name')}',
+                    style: getTitleStyle(color: Appcolors.white),
+                  ),
+                  Text(
+                    'Have a nice day',
+                    style: getBodyStyle(color: Appcolors.grey),
+                  ),
+                ]),
+                const Spacer(),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(360),
+                    child: Image.file(File(AppLocalStorage.getData('path')),
+                        width: 60, height: 60, fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                      return Image.asset('assets/user.png');
+                    })),
+              ]),
+              const Gap(10),
+              CarouselSlider.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int itemIndex,
+                          int pageViewIndex) =>
+                      Image.asset(
+                        'assets/rodri.png',
+                        height: 200,
+                      ),
+                  options: CarouselOptions(
+                    height: 200,
+                    viewportFraction: 0.75,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        page = index;
+                      });
+                    },
+                    autoPlay: true,
+                    scrollDirection: Axis.horizontal,
+                  )),
+              SmoothPageIndicator(
+                controller: PageController(initialPage: page), // PageController
+                count: 5,
+                effect: ScrollingDotsEffect(
+                  activeDotColor: Appcolors.green,
+                  dotColor: Colors.grey,
+                  dotHeight: 10,
+                  dotWidth: 10
+                ), // your preferred effect
+              )
+            ]),
           ),
         ),
       ),
